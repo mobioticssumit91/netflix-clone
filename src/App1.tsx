de37297a826b52ff0eb4ace2670576265bedbfe7
sumit kumar dey <sumitkumardey91@yahoo.com>
2024-03-31 21:20:33 +0530

    removed

--- src/App1.tsx
+++ src/App1.tsx
@@ -19,7 +19,6 @@ function App() {
     setArr(arr.map((item, index) => {
       if (index == itemIndex) {
         // item.isChecked = event.target.checked;
-        //test
       }
       return item;
     }))

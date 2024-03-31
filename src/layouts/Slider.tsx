4dfa6f8783cb871908e7ce28df452d8b17c92c60
sumit kumar dey <sumitkumardey91@yahoo.com>
2024-03-31 21:04:09 +0530

    updated

--- src/layouts/Slider.tsx
+++ src/layouts/Slider.tsx
@@ -2,12 +2,14 @@ import SingleCard from "../components/SingleCard";
 
 
 
+// @ts-expect-error
+
 function Slider({imageList, title}) {
     return (
         <div className="wrapper-slider">
             <div className="slider-title"> {title} </div>
             <div className="slider-manager">
-                {imageList.map((list: object, index: number) => <SingleCard image={list} key={index}></SingleCard>)}
+                {imageList.map((list: string, index: number) => <SingleCard image={list} key={index}></SingleCard>)}
                
             </div>
         </div>
